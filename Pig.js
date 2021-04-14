ba892ccd9a6bcbd4bf1c1078c5e86fe8560bf334
Codingstoff <73205292+Codingstoff@users.noreply.github.com>
2021-04-14 12:50:50 -0500

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,26 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.visiblity=255;
+    this.image = loadImage("sprites/enemy.png");
+  }
+display()
+{
+  var position = this.body.position
+  if(this.body.speed>5)
+  {
+    World.remove(world, this.body);
+    push();
+    tint(255,this.visiblity);
+    image(this.image,position.x,position.y,50,50)
+    this.visiblity = this.visiblity - 5;
+    pop();
+  }
+  else
+  {
+    super.display();
+  }
+
+
+}
+}
